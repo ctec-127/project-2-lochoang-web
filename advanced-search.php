9931280e8c762eb0b82e6b9933ce9b43eff95dae
Loc Hoang <l.hoang2@students.clark.edu>
2022-04-22 13:07:25 -0400

    commit

--- advanced-search.php
+++ advanced-search.php
@@ -6,7 +6,7 @@ require_once 'inc/layout/header.inc.php';
 <div class="container-fluid">
 	<div class="row mt-5">
 		<div class="col-12 col-sm-12 col-md-3 col-lg-3 mb-5 border-right border-secondary p-2">
-			<h1>Branch 1 Advanced Search</h1>
+			<h1>Branch 1- Advanced Search</h1>
 			<?php require_once __DIR__ . '/inc/advanced-search/advanced-search.inc.php'; ?>
 			<?php require_once __DIR__ . '/inc/shared/form.inc.php' ?>
 		</div>
