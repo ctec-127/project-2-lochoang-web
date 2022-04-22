02a4de547abb9d1ddc5055839e52bc737517c042
Loc Hoang <l.hoang2@students.clark.edu>
2022-04-22 12:34:38 -0400

    Updated_1.0

--- advanced-search.php
+++ advanced-search.php
@@ -2,18 +2,18 @@
 $pageTitle = "Advanced Search";
 require_once 'inc/layout/header.inc.php';
 ?>
-
-<div class="container-fluid">
-	<div class="row mt-5">
-		<div class="col-12 col-sm-12 col-md-3 col-lg-3 mb-5 border-right border-secondary p-2">
-			<h1>Advanced Search</h1>
-			<?php require_once __DIR__ . '/inc/advanced-search/advanced-search.inc.php'; ?>
-			<?php require_once __DIR__ . '/inc/shared/form.inc.php' ?>
+<h1>This is branch 1 update<h1>
+		<div class="container-fluid">
+			<div class="row mt-5">
+				<div class="col-12 col-sm-12 col-md-3 col-lg-3 mb-5 border-right border-secondary p-2">
+					<h1>Advanced Search</h1>
+					<?php require_once __DIR__ . '/inc/advanced-search/advanced-search.inc.php'; ?>
+					<?php require_once __DIR__ . '/inc/shared/form.inc.php' ?>
+				</div>
+				<div class="col-12 col-sm-12 col-md-9 col-lg-9">
+					<?php require_once __DIR__ . '/inc/advanced-search/advanced-search-results.inc.php'; ?>
+				</div>
+			</div>
 		</div>
-		<div class="col-12 col-sm-12 col-md-9 col-lg-9">
-			<?php require_once __DIR__ . '/inc/advanced-search/advanced-search-results.inc.php'; ?>
-		</div>
-	</div>
-</div>
 
-<?php require_once 'inc/layout/footer.inc.php'; ?>
+		<?php require_once 'inc/layout/footer.inc.php'; ?>
